0846d5e6a3571ce636269859bc343c70013e5a18
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-25 21:57:32 +0300

    feat: add simple counter with two buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Redux Sandbox');
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    default: 
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'DEC' });
+  });
+
+  document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({ type: 'INC' });
+  });
+
+const update = () => {
+  document
+    .getElementById('counter')
+    .innerText = store.getState();
+};
+
+store.subscribe(update);
